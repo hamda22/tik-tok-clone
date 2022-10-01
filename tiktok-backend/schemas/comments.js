e9f683820230f286feec51882e99f35630f2b051
hamda22 <xamdacilmi@gmail.com>
2022-10-01 13:48:35 +0300

    created schemas for the project using sanity

---
+++ tiktok-backend/schemas/comments.js
@@ -0,0 +1,19 @@
+export default {
+    name:'comment',
+    title:'Comment',
+    type:'document',
+    fields:[
+{
+    name:'PostedBy',
+    title:'Posted By',
+    type:'postedBy'
+},
+{ 
+
+    name:'comment',
+    title:'Comment',
+    type:'string',
+
+}
+  ]
+}
